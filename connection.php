cad3116221de00bec81d816dd70fecf34556d557
clever matchless bravo villafuerte <cleveresclip@hotmail.com>
2021-10-07 17:57:55 -0400

    detail

--- connection.php
+++ connection.php
@@ -4,7 +4,7 @@ $host=getenv("MYSQL_HOST");
 $username=getenv("MYSQL_USERNAME");
 $password=getenv("MYSQL_PASSWORD");
 $db=getenv("MYSQL_DB");
-echo "trying to connect.";
+echo "trying to connect.$host $username $password $db";
 $connection=new mysqli($host,$username,$password,$db);
 if($connection->connect_errno)
 {
