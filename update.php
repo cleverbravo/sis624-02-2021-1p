616d6879e453066188114517280f0b2ce2c9e818
clever matchless bravo villafuerte <cleveresclip@hotmail.com>
2021-10-07 18:09:00 -0400

    d

--- update.php
+++ update.php
@@ -5,7 +5,7 @@ $command="update estudiante set nombre='juan dos',ci='1234',telf='4321' where no
 $result=$connection->query($command);
 
 if($result=$connection->query($command)){
-    echo "todo correcto."
+    echo "todo correcto.";
 }
 else{
     echo "algun error.".$connection->error;
